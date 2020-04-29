739cd56caa2b0f9150226e21202fedb6c317bb68
dschmitz89 <danielschmitzsiegen@googlemail.com>
2020-04-29 09:29:59 +0200

    Clean setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 #from setuptools import setup
  
 setup(
-    name='spherical_stats',    # This is the name of your PyPI-package.
+    name='spherical_stats',   
     version='0.2', 
-    description='Numba based spherical statistics',
+    description='Spherical statistics in Python',
     author='Daniel Schmitz',
-    license='MIT',                        # Update the version number for new releases
+    license='MIT',
     packages=['spherical_stats'],
     url='https://github.com/dschmitz89/spherical_stats',
     install_requires=[
         'numpy',
         'numba>0.44',
         'scipy>0.11'
-    ]              # The name of your scipt, and also the command you'll be using for calling it
+    ]             
 )
