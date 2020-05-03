eec571ad419c4fd2c2e0a0a21ea1d7967fbe7d90
dschmitz89 <danielschmitzsiegen@googlemail.com>
2020-05-03 13:07:13 +0200

    Add orientation matrix and random symmetric pos. definite matrix generation

---
+++ spherical_stats/utils.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 15:22:05 2020
+
+@author: tyrion
+"""
+import numpy as np
+
+def random_spd_matrix(normalize = True):
+
+    A = np.random.rand(3,3)
+
+    B = np.dot(A, A.T)
+    trace = np.trace(B)
+
+    if normalize:
+
+        B = 3 * B/trace
+    
+    return B
+
+def orientation_matrix(vectors, eigen = True):
+    '''
+    Calculates the orientation matrix and its Eigen decomposition
+    of a set of vectors
+
+    Parameters
+    ----------
+    vectors : ndarray [n,3]
+        sample of n vectors
+    eigen : bool, optional
+        indicate that Eigen decomposition shall be performed. 
+        The default is True.
+
+    Returns
+    -------
+    orientation_matrix
+    ndarray [3,3,]
+    
+    or
+    
+    eigenvals: ndarray[3]
+    eigenvecs: ndarray[3,3]
+
+    '''
+    x_vals = vectors[:, 0]
+    y_vals = vectors[:, 1]
+    z_vals = vectors[:, 2]
+    
+    orientation_matrix = np.zeros((3,3))
+    
+    orientation_matrix[0, 0] = np.sum(x_vals * x_vals)
+    xysum = np.sum(x_vals * y_vals)
+    orientation_matrix[0, 1] = xysum
+    orientation_matrix[1, 0] = xysum
+    xzsum = np.sum(x_vals * z_vals)
+    orientation_matrix[0, 2] = xzsum
+    orientation_matrix[2, 0] = xzsum
+    orientation_matrix[1, 1] = np.sum(y_vals * y_vals)
+    yzsum = np.sum(y_vals * z_vals)
+    orientation_matrix[1, 2] = yzsum
+    orientation_matrix[2, 1] = yzsum
+    orientation_matrix[2, 2] = np.sum(z_vals * z_vals)
+    
+    if eigen == True:
+        
+        eigenvals, eigenvecs = np.linalg.eigh(orientation_matrix)
+        
+        n_samples = vectors.shape[0]
+        
+        eigenvals = eigenvals/n_samples
+        
+        return eigenvals, eigenvecs
+    
+    else:
+        
+        return orientation_matrix    
