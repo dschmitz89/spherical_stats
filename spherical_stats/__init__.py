5cb33848c670ea6635bab99b5cddf34841bfb52b
dschmitz89 <danielschmitzsiegen@gmail.com>
2021-09-05 18:05:31 +0200

    Remove files and insert again with underscore for packaging

--- spherical_stats/__init__.py
+++ spherical_stats/__init__.py
@@ -1,6 +1,21 @@
-from .esag import ESAG
-from .acg import ACG
-from .plot_utils import sphere, spherical_hist
-from .coordinate_systems import *
-from .descriptive_stats import *
-from .utils import *
+from ._esag import ESAG
+from ._acg import ACG
+from ._plot_utils import sphere, spherical_hist
+from ._coordinate_systems import vectors_to_polar, polar_to_vectors, vectors_to_geographical, geographical_to_vectors
+from ._descriptive_stats import spherical_mean, spherical_variance, orientation_matrix
+from ._utils import sphericalrand
+
+__all__ = [
+    ACG,
+    ESAG,
+    sphere,
+    spherical_hist,
+    spherical_mean,
+    spherical_variance,
+    vectors_to_polar,
+    polar_to_vectors,
+    vectors_to_geographical,
+    geographical_to_vectors,
+    orientation_matrix,
+    sphericalrand,
+]
