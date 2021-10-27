227e8d1e3e8e031a39c1d32f50c789ec004f8213
dschmitz89 <danielschmitzsiegen@gmail.com>
2021-10-27 21:03:34 +0200

    Bump version to 1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
  
 setup(
     name='spherical_stats',   
-    version='1.1', 
+    version='1.2', 
     description='Spherical statistics in Python',
     author='Daniel Schmitz',
     author_email='danielschmitzsiegen@gmail.com',
