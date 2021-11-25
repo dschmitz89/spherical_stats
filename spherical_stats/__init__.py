b697766f951190d92f6142d0422653c95c47dc83
dschmitz89 <danielschmitzsiegen@gmail.com>
2021-11-25 14:28:22 +0100

    Add Watson distribution

--- spherical_stats/__init__.py
+++ spherical_stats/__init__.py
@@ -1,12 +1,14 @@
 from ._esag import ESAG
 from ._acg import ACG
 from ._vmf import VMF
+from ._watson import Watson
 from ._plot_utils import sphere, spherical_hist, evaluate_on_sphere
 from ._coordinate_systems import vectors_to_polar, polar_to_vectors, vectors_to_geographical, geographical_to_vectors
 from ._descriptive_stats import spherical_mean, spherical_variance, orientation_matrix
 from ._utils import sphericalrand, load_northpole
 
 __all__ = [
+    "Watson",
     "ACG",
     "ESAG",
     "VMF",
